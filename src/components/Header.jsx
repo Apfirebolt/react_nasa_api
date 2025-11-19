7f51ec040b64c18a729f0114c5119b6f0247a9de
Apfirebolt <aspper20@gmail.com>
2025-11-19 23:46:57 +0530

    feat: added dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,42 @@
 import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
+import { RocketOutlined, HistoryOutlined, DatabaseOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
 
 const menuItems = [
   { key: "1", label: <Link to="/">Home</Link> },
-  { key: "2", label: <Link to="/spaceship">Spaceship</Link> },
-  { key: "3", label: <Link to="/launches">Launches</Link> },
-  { key: "4", label: <Link to="/capsules">Capsules</Link> },
-  { key: "5", label: <Link to="/cores">Cores</Link> },
-  { key: "6", label: <Link to="/payloads">Payloads</Link> },
-  { key: "7", label: <Link to="/missions">Missions</Link> },
-  { key: "8", label: <Link to="/rockets">Rockets</Link> },
-  { key: "9", label: <Link to="/dragons">Dragons</Link> },
-  { key: "10", label: <Link to="/history">History</Link> },
-  { key: "11", label: <Link to="/landpads">Landing Pads</Link> },
+  {
+    key: "spacecraft",
+    label: "Spacecraft",
+    icon: <RocketOutlined />,
+    children: [
+      { key: "2", label: <Link to="/spaceship">Spaceship</Link> },
+      { key: "8", label: <Link to="/rockets">Rockets</Link> },
+      { key: "9", label: <Link to="/dragons">Dragons</Link> },
+      { key: "4", label: <Link to="/capsules">Capsules</Link> },
+    ],
+  },
+  {
+    key: "operations",
+    label: "Operations",
+    icon: <DatabaseOutlined />,
+    children: [
+      { key: "3", label: <Link to="/launches">Launches</Link> },
+      { key: "5", label: <Link to="/cores">Cores</Link> },
+      { key: "6", label: <Link to="/payloads">Payloads</Link> },
+      { key: "11", label: <Link to="/landpads">Landing Pads</Link> },
+    ],
+  },
+  {
+    key: "info",
+    label: "Information",
+    icon: <HistoryOutlined />,
+    children: [
+      { key: "7", label: <Link to="/missions">Missions</Link> },
+      { key: "10", label: <Link to="/history">History</Link> },
+    ],
+  },
 ];
 
 const AppHeader = () => {
@@ -26,6 +48,8 @@ const AppHeader = () => {
         mode="horizontal"
         defaultSelectedKeys={["1"]}
         items={menuItems}
+        triggerSubMenuAction="hover"
+        popupClassName="full-width-submenu"
       />
     </Header>
   );
